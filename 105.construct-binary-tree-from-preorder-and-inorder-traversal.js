b2bc4d5d4e9e6856162ae89d711f5d616c0f8129
Sim <sim@snorl.ax>
2021-11-20 23:21:52 +0800

    105 - iterative solution

--- 105.construct-binary-tree-from-preorder-and-inorder-traversal.js
+++ 105.construct-binary-tree-from-preorder-and-inorder-traversal.js
@@ -13,6 +13,34 @@ var buildTree = function(preorder, inorder) {
     let right = buildTree(preorder.slice(1+rootInOrderIndex), inorder.slice(rootInOrderIndex+1))
     return new TreeNode(preorder[0], left, right)
 };
+// 2. iterative
+var buildTree = function(preorder, inorder) {
+    if(!preorder.length || !inorder.length) {
+        return null
+    }
+    let stack = [], inOrderMap = new Map()
+    for(let [index, node] of inorder.entries()) {
+        inOrderMap.set(node, index)
+    }
+    let root = new TreeNode(0)
+    stack.push([root, 0, preorder.length-1])
+    for(let i = 0; i < preorder.length; i++) {
+        let [cur, low, high] = stack.pop()
+        cur.val = preorder[i]
+        let inOrderIndex = inOrderMap.get(preorder[i])
+        if(inOrderIndex < high) {
+            let rightMid = new TreeNode(0)
+            cur.right = rightMid
+            stack.push([rightMid, inOrderIndex+1, high])
+        }
+        if(low < inOrderIndex) {
+            let leftMid = new TreeNode(0)
+            cur.left = leftMid
+            stack.push([leftMid, low, inOrderIndex-1])
+        }
+    }
+    return root
+};
 // @lc code=start
 /**
  * Definition for a binary tree node.
@@ -31,10 +59,28 @@ var buildTree = function(preorder, inorder) {
     if(!preorder.length || !inorder.length) {
         return null
     }
-    let rootInOrderIndex = inorder.indexOf(preorder[0])
-    let left = buildTree(preorder.slice(1, 1+rootInOrderIndex), inorder.slice(0, rootInOrderIndex))
-    let right = buildTree(preorder.slice(1+rootInOrderIndex), inorder.slice(rootInOrderIndex+1))
-    return new TreeNode(preorder[0], left, right)
+    let stack = [], inOrderMap = new Map()
+    for(let [index, node] of inorder.entries()) {
+        inOrderMap.set(node, index)
+    }
+    let root = new TreeNode(0)
+    stack.push([root, 0, preorder.length-1])
+    for(let i = 0; i < preorder.length; i++) {
+        let [cur, low, high] = stack.pop()
+        cur.val = preorder[i]
+        let inOrderIndex = inOrderMap.get(preorder[i])
+        if(inOrderIndex < high) {
+            let rightMid = new TreeNode(0)
+            cur.right = rightMid
+            stack.push([rightMid, inOrderIndex+1, high])
+        }
+        if(low < inOrderIndex) {
+            let leftMid = new TreeNode(0)
+            cur.left = leftMid
+            stack.push([leftMid, low, inOrderIndex-1])
+        }
+    }
+    return root
 };
 // @lc code=end
 
