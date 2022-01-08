99140939a9bb406ead7b2545e455a8af32f99743
Sim <sim@snorl.ax>
2022-01-08 23:31:05 +0800

    [138] Copy List with Random Pointer

--- 138.copy-list-with-random-pointer.js
+++ 138.copy-list-with-random-pointer.js
@@ -3,6 +3,9 @@
  *
  * [138] Copy List with Random Pointer
  */
+
+const { isInt32Array } = require("util/types");
+
 // 1. recursive
 var copyRandomList = function(head) {
     const copies = new Map()
@@ -43,6 +46,63 @@ var copyRandomList = function(head) {
     }
     return copies.get(head)
 };
+//3. no hashmap (insert copy behind)
+var copyRandomList = function(head) {
+    if(!head) {
+        return head
+    }
+    let l1 = head
+    let l2
+    while(l1) {
+        l2 = l1.next
+        l1.next = new Node(l1.val)
+        l1.next.next = l2
+        l1 = l2
+    }
+    let final_head = head.next
+
+    for(l1 = head; l1; l1 = l1.next.next) {
+        if(l1.random) {
+            l1.next.random = l1.random.next
+        }
+    }
+    
+    for(l1 = head; l1; l1 = l1.next) {
+        l2 = l1.next
+        if(l2) {
+            l1.next = l2.next
+        }
+        
+        if(l2?.next) {
+            l2.next = l2.next.next
+        }
+    }
+
+    return final_head
+};
+// 4. no hashmap (random and next exchanged)
+var copyRandomList = function(head) {
+    if(!head) {
+        return head
+    }
+    let l1, l2
+    for(l1 = head; l1; l1 = l1.next) {
+        l2 = new Node(l1.val)
+        l2.next = l1.random
+        l1.random = l2
+    }
+    for(l1 = head; l1; l1 = l1.next) {
+        l2 = l1.random
+        l2.random = l2.next?.random || null
+    }
+    let res = head.random
+    for(l1 = head; l1; l1 = l1.next) {
+        l2 = l1.random
+        l1.random = l2.next
+        l2.next = l1.next?.random || null
+    }
+    return res
+};
 // @lc code=start
 /**
  * // Definition for a Node.
@@ -61,20 +121,23 @@ var copyRandomList = function(head) {
     if(!head) {
         return head
     }
-    const copies = new Map()
-    let m = head
-    while(m) {
-        copies.set(m, new Node(m.val))
-        m = m.next
+    let l1, l2
+    for(l1 = head; l1; l1 = l1.next) {
+        l2 = new Node(l1.val)
+        l2.next = l1.random
+        l1.random = l2
     }
-    m = head
-    while(m) {
-        let copied = copies.get(m)
-        copied.next = copies.get(m.next) || null
-        copied.random = copies.get(m.random) || null
-        m = m.next
+    for(l1 = head; l1; l1 = l1.next) {
+        l2 = l1.random
+        l2.random = l2.next?.random || null
     }
-    return copies.get(head)
+    let res = head.random
+    for(l1 = head; l1; l1 = l1.next) {
+        l2 = l1.random
+        l1.random = l2.next
+        l2.next = l1.next?.random || null
+    }
+    return res
 };
 // @lc code=end
 
